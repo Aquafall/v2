15a939fdb45bdcc94d4eb0ac74d4a4ee3e0c052b
Logan Cookson <66440439+logangamesdaily@users.noreply.github.com>
2025-11-24 21:53:04 +0000

    Wah

--- aquafall2/vite.config.js
+++ aquafall2/vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  base: "./",
   server: {
     allowedHosts: [
       "aud.is-a.dev",
